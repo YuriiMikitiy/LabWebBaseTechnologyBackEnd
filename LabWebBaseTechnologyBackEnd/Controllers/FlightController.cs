9642d3d3905423a91558e9a33602059be1faa0e2
mikitiiiura <yura.mikitiy@gmail.com>
2025-09-17 19:32:25 +0300

    push lab6

--- LabWebBaseTechnologyBackEnd/Controllers/FlightController.cs
+++ LabWebBaseTechnologyBackEnd/Controllers/FlightController.cs
@@ -1,6 +1,8 @@
-﻿using LabWebBaseTechnologyBackEnd.DataAccess.ModulEntity;
+﻿using LabWebBaseTechnologyBackEnd.DataAccess;
+using LabWebBaseTechnologyBackEnd.DataAccess.ModulEntity;
 using LabWebBaseTechnologyBackEnd.DataAccess.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace LabWebBaseTechnologyBackEnd.Controllers
 {
@@ -9,18 +11,42 @@ namespace LabWebBaseTechnologyBackEnd.Controllers
     public class FlightController : ControllerBase
     {
         private readonly IFlightRepository _flightRepository;
+        private readonly LabWebBaseTechnologyDBContext _context;
 
-        public FlightController(IFlightRepository flightRepository)
+        public FlightController(IFlightRepository flightRepository, LabWebBaseTechnologyDBContext dBContext)
         {
             _flightRepository = flightRepository;
+            _context = dBContext;
         }
 
-        [HttpGet]
-        public async Task<ActionResult<List<FlightEntity>>> GetBook()
-        {
-            var book = await _flightRepository.GetAll();
+        //[HttpGet]
+        //public async Task<ActionResult<List<FlightEntity>>> GetBook()
+        //{
+        //    var book = await _flightRepository.GetAll();
 
-            return Ok(book);
+        //    return Ok(book);
+        //}
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<FlightEntity>>> GetFlights()
+        {
+            return await _context.Flights.ToListAsync();
+        }
+
+        [HttpGet("data")]
+        public async Task<ActionResult<object>> GetTrainingData()
+        {
+            var flights = await _context.Flights
+                .Include(f => f.DelayData)
+                .ToListAsync();
+
+            var data = flights.Select(f => new
+            {
+                Weather = f.DelayData.FirstOrDefault()?.Weather ?? "Clear",
+                DelayProbability = f.DelayData.FirstOrDefault()?.DelayProbability ?? 0.0,
+                Status = f.Status
+            }).ToList();
+
+            return Ok(data);
         }
 
     }
